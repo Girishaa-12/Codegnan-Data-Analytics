ac6959c9528c471c95344ab1c53badb871b4f93c
Thangavalu Girisha <venkateshgirisha12@gmail.com>
2026-05-13 15:44:26 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,40 @@
+Introduction to Python:
+
+"""Today I started learning Python as part of my Data Analytics course at Codegnan.
+I learned the basic concepts of Python programming and practiced simple programs.
+#Python is a high-level,Dynamically Typed lang, Interpreter lang 
+It is widely used in:
+Data Analytics
+Artificial Intelligence
+Web Development
+Automation
+Machine Learning
+Python is beginner-friendly because of its simple syntax.
+
+Example:
+print("Hello World")"""
+#Dynamically typed lang
+"""
+num = 23
+so = "python"
+any = [1,2]
+an = {"Name" : "Girisha"}
+print(num)
+print(so)
+print(any)
+print(an)
+"""
+#Create a 3 diff variables
+"""
+Name="Thangavalu_Girisha"
+rollno=9
+print(Name)
+print(rollno)
+"""
+#Boolean
+a = 5
+b = 6
+print(a==b)
+
+
+
